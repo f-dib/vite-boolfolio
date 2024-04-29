6eb3b28d63cdeb1a496c63ee472db412308873c1
federico-dibitonto <f.dibitonto94@gmail.com>
2024-04-29 18:08:55 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import "bootstrap/dist/css/bootstrap.css";
+import './style.scss'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
