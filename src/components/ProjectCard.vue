fdb18c61e4000d04f0e06ba017f1e273362258a3
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-04-30 18:27:23 +0200

    style cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -20,8 +20,19 @@ export default {
   <div class="container py-5">
 
     <div class="card">
+      
+      <img src="" alt="{{ project.name }}">
+      <h1>
+        {{ project.name }}
+      </h1>
+      <h4>
+        {{ project.description }}
+      </h4>
+      <h6>
+        {{ project.date }}
+      </h6>
 
-      {{ project.name }}
+
 
     </div>
 
@@ -32,5 +43,26 @@ export default {
 
 <style lang="scss">
 
+.container {
+    padding: 20px;
+  }
+
+  .card {
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    padding: 20px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  }
+
+  .card img {
+    width: 100%;
+    max-height: 200px;
+    object-fit: cover;
+  }
+
+  h1, h4, h6 {
+    margin: 5px 0;
+  }
 
 </style>
