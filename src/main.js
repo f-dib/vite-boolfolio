36552affe3e1eed1f7617444b7302a8d5334fe67
federico-dibitonto <f.dibitonto94@gmail.com>
2024-05-01 02:50:36 +0200

    fixed main.js and showed homepage

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue'
-import "bootstrap/dist/css/bootstrap.css";
 import './style.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
-
 import { router } from './router'
 
-import "bootstrap/dist/js/bootstrap.js";
+import * as bootstrap from 'bootstrap'
 
 createApp(App).use(router).mount('#app')
-
-
