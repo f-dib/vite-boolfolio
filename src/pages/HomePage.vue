385959d0bbbf86e1a9106eef500fea706e0c51bc
federico-dibitonto <f.dibitonto94@gmail.com>
2024-05-02 17:52:14 +0200

    fixed card and removed list

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -155,22 +155,36 @@ export default {
 
     <h1>I nostri progetti</h1>
 
+        <!-- Seconda sezione -->
+        <div class="container mt-5 pt-5 text-center">
+          <div class="row">
+            <div class="col-md-12">
+                <h2 class="center-content">Titolo Secondario</h2>
+                <p class="center-content">Testo del paragrafo</p>
+            </div>
+          </div>
+        </div>
+
     <div v-if="!isLoading">
 
-      <ul>
-
-        <li v-for="project in projects" class="mb-2">
-
-          {{ project.name }} 
-          
-          <router-link :to="{name: 'single-project', params: {slug: project.slug}}" class="btn btn-outline-info btn-outline">
-            Mostra
-          </router-link>
-
-        </li>
-
-      </ul>
-
+      <div class="row">
+        <div class="d-flex gap-3 m-auto justify-content-around">
+          <div v-for="project in projects"  class="mt-3">      
+            <div class="col">
+                <div class="card">
+                  <img :src="'http://localhost:8000/storage/' + project.src" class="card-img-top" alt="...">
+                  <div class="card-body">
+                      <h5 class="card-title">{{ project.name }}</h5>
+                      <p class="card-text">Contenuto della card 1.</p>
+                  </div>
+                  <router-link :to="{name: 'single-project', params: {slug: project.slug}}" class="btn btn-outline-info btn-outline">
+                  Mostra
+                  </router-link>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
 
@@ -216,49 +230,6 @@ export default {
               <button class="btn btn-sm btn-outline-light">Button</button>
           </div>
           </div>
-      </div>
-
-      <!-- Seconda sezione -->
-      <div class="container mt-5 pt-5 text-center">
-          <div class="row">
-          <div class="col-md-12">
-              <h2 class="center-content">Titolo Secondario</h2>
-              <p class="center-content">Testo del paragrafo</p>
-          </div>
-          </div>
-          <div class="row mt-3">
-
-          <!-- Card 1 -->
-          <div class="card col-md-4">
-              <div class="card">
-              <div class="card-body">
-                  
-                  <h5 class="card-title">Card 1</h5>
-                  <p class="card-text">Contenuto della card 1.</p>
-              </div>
-              </div>
-          </div>
-          <!-- Card 2 -->
-          <div class="card col-md-4">
-              <div class="card">
-              <div class="card-body">
-                  <h5 class="card-title">Card 2</h5>
-                  <p class="card-text">Contenuto della card 2.</p>
-              </div>
-              </div>
-          </div>
-          <!-- Card 3 -->
-          <div class="card col-md-4">
-              <div class="card">
-              <div class="card-body">
-                  <h5 class="card-title">Card 3</h5>
-                  <p class="card-text">Contenuto della card 3.</p>
-              </div>
-              </div>
-          </div>
-          </div>
-
-          
       </div>
 
 
