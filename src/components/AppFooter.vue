41f1ccb728fe19f2fd57ff8e1db9efb0e2928697
mario.arista <mario.arista.dev@gmail.com>
2024-05-02 16:35:51 +0200

    footer basic done

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,13 +8,28 @@ export default {
 <template>
 
     <footer>
-        <div class="container py-5 bg-red">
+        <div class="container py-5">
+
+            <!-- Per ora come info mail e tel  -->
 
             <div class="d-flex justify-content-between align-items-center">
 
-                <div class="contacts-footer">
-                    <h3>CONTACTS:</h3>
-                    
+                <div class="contacts-footer d-flex flex-column">
+                    <div>CONTACTS</div>
+                    <div>
+                        <a class="text-decoration-none" href="mailto:mario@gmail.com">mario@gmail.com</a>
+                    </div>
+                    <div>
+                        <a class="text-decoration-none" href="tel:+39166123456">166-123456</a>
+                    </div>
+                </div>
+
+                <!-- Per ora di messe icone di prova -->
+
+                <div class="socials-footer d-flex gap-3">
+                    <i class="fa-brands fa-linkedin"></i>
+                    <i class="fa-brands fa-github"></i>
+                    <i class="fa-brands fa-behance"></i>
                 </div>
 
             </div>
@@ -26,9 +41,13 @@ export default {
 </template>
 
 <style lang="scss">
-nav {
-    .container {
-        border-bottom: 1px solid white;
+
+    footer {
+        position: absolute;
+        width: 100%;
+        bottom: 0px;
+        left: 0px;
+
     }
-}
+
 </style>
