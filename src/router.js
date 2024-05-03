81bb443c64e022afe5fffc43f23650bbf0f42c1c
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-05-03 13:01:36 +0200

    Email-frontend

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import SingleProject from './pages/SingleProject.vue';
 import Projects from './pages/Projects.vue';
+import ContactMe from './pages/ContactMe.vue';
 
 
 const router = createRouter({
@@ -26,6 +27,12 @@ const router = createRouter({
             path: '/projects/:slug',
             name: 'single-project',
             component: SingleProject
+        },
+
+        {
+            path: '/contact',
+            name: 'contact',
+            component: ContactMe
         }
     ]
 
